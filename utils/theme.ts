00bb53c4efc4a6b13bf16c7c55a605988e83bbde
MasterKenth <MasterKenth@users.noreply.github.com>
2022-05-13 20:35:16 +0200

    🧹 chore: cleanup and add boilerplate

---
+++ utils/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    text: "#111",
+    background: "#300",
+    primary: "tomato",
+    secondary: "#3f3f3f",
+    muted: "#e0e0e0",
+    highlight: "#9f9f9f",
+    gray: "#6c6c6c",
+    accent: "#3f3f3f",
+  },
+  fonts: {
+    body: "Comic Sans MS",
+  },
+};
+
+export default theme;
