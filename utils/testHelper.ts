fad324a5a95521b1770f14614f4e9e0aba2da49c
MasterKenth <MasterKenth@users.noreply.github.com>
2022-07-10 01:28:06 +0200

    🧪 test(mock): change mock helper for better variety

--- utils/testHelper.ts
+++ utils/testHelper.ts
@@ -7,6 +7,9 @@ import type { PokemonInfo, Nature, MoveInfo } from "./Generator/pokemon.types";
 
 import Pikachu from "../public/data/pokemon/25.json";
 import Pound from "../public/data/moves/1.json";
+import KarateChop from "../public/data/moves/2.json";
+import Fly from "../public/data/moves/19.json";
+import Thunder from "../public/data/moves/87.json";
 import Natures from "../public/data/naturesList.json";
 import Abilities from "../public/data/abilitiesList.json";
 
@@ -15,13 +18,15 @@ export const MOCK_PROP: Required<GameProp> = {
   optionalProp: "optionalMock",
 };
 
+export const MOCK_NATURE = Natures[15] as Nature;
+
 export const mockPropGetter: GamePropGetter<GeneratedPokemon> = (_) =>
   MOCK_PROP;
 
 export const mockPokemon: GeneratedPokemon = {
   info: Pikachu as PokemonInfo,
-  nature: Natures[6] as Nature,
+  nature: MOCK_NATURE,
   showdownExport: "",
   ability: Abilities[0],
-  moves: [Pound, Pound, Pound, Pound] as MoveInfo[],
+  moves: [Pound, KarateChop, Fly, Thunder] as MoveInfo[],
 };
