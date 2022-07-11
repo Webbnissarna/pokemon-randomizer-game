09ebb3112a6bb658f3024904e6e081600ce8b18c
MasterKenth <MasterKenth@users.noreply.github.com>
2022-07-11 23:06:03 +0200

    🧪 test: change test mock data to not depend on external scraped data

--- utils/testHelper.ts
+++ utils/testHelper.ts
@@ -1,32 +1,37 @@
+import mockPokemonData from "./testMockData/pokemon.json";
+import mockAbilityData from "./testMockData/ability.json";
+import mockNatureData from "./testMockData/nature.json";
+import mockMove1 from "./testMockData/move1.json";
+import mockMove2 from "./testMockData/move2.json";
+import mockMove3 from "./testMockData/move3.json";
+import mockMove4 from "./testMockData/move4.json";
+
 import type {
   GameProp,
   GamePropGetter,
 } from "./GamePropSystem/GamePropSystem.types";
 import type { GeneratedPokemon } from "./Generator/generator.types";
-import type { PokemonInfo, Nature, MoveInfo } from "./Generator/pokemon.types";
-
-import Pikachu from "../public/data/pokemon/25.json";
-import Pound from "../public/data/moves/1.json";
-import KarateChop from "../public/data/moves/2.json";
-import Fly from "../public/data/moves/19.json";
-import Thunder from "../public/data/moves/87.json";
-import Natures from "../public/data/naturesList.json";
-import Abilities from "../public/data/abilitiesList.json";
+import type {
+  PokemonInfo,
+  Nature,
+  MoveInfo,
+  Ability,
+} from "./Generator/pokemon.types";
 
 export const MOCK_PROP: Required<GameProp> = {
   mainProp: "mainMock",
   optionalProp: "optionalMock",
 };
 
-export const MOCK_NATURE = Natures[15] as Nature;
+export const MOCK_NATURE = mockNatureData as Nature;
 
 export const mockPropGetter: GamePropGetter<GeneratedPokemon> = (_) =>
   MOCK_PROP;
 
 export const mockPokemon: GeneratedPokemon = {
-  info: Pikachu as PokemonInfo,
+  info: mockPokemonData as PokemonInfo,
   nature: MOCK_NATURE,
   showdownExport: "",
-  ability: Abilities[0],
-  moves: [Pound, KarateChop, Fly, Thunder] as MoveInfo[],
+  ability: mockAbilityData as Ability,
+  moves: [mockMove1, mockMove2, mockMove3, mockMove4] as MoveInfo[],
 };
